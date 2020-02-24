85db26c70c1e2d62350552f443dd93dfee829a5f
HannahHans1 <61013931+HannahHans1@users.noreply.github.com>
2020-02-24 15:05:13 +0100

    Update elevator.h

--- elevator.h
+++ elevator.h
@@ -10,8 +10,10 @@
 * 
 */
 
-
-
+/**
+* @brief Definerer en enum med mulige retninger 
+* heisen kan ha
+*/
 
 
 typedef enum {
@@ -23,18 +25,53 @@ extern int last_floor;
 extern elevator_direction last_dir;
 extern HardwareMovement movement;
 
+
+
+/** 
+* @brief Gets last direction of elevator
+*/
 elevator_direction elevator_get_last_direction();
 
+/**
+* @brief Updates last direction of elevator
+* 
+* @param f Last floor
+*/
 void elevator_update_last_floor(int f);
 
+/**
+* @brief Returns last floor
+*
+*/
 int elevator_get_last_floor();
 
+/**
+* @brief Decides new motor direction
+* 
+* @param new_movement The new direction of 
+* the motor
+*
+*/
 void elevator_set_motor_direction(HardwareMovement new_movement);
 
+/**
+* @brief checks whether the elevator is at
+* a floor, and updates which floor was the 
+* previous floor.
+*
+*/
 int elevator_at_floor();
 
+/**
+* @brief Stops the motor of the elevator
+*/
 void elevator_stop_at_floor();
 
+/**
+* @brief Orders the elevator down if it's at the
+* top, and up if it's at the bottom.
+*
+*/
 void elevator_at_ends();
 
 
