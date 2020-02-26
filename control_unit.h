9c3e54885bdd2a54268e9040a679343f62f6c1f5
supermina123 <havermina@gmail.com>
2020-02-26 11:03:00 +0100

    doxygen

--- control_unit.h
+++ control_unit.h
@@ -20,33 +20,67 @@ int next_in_queue;
 * @brief Decides what to do with the next 
 * order at a given floor
 * 
-* @param orders
-* @param state
+* @param orders An element of elevator_orders providing
+* the order table and priority queue.
+* @param controller An element of elevator_controller
+* providing the previous direction, previous floor and
+* the present state.
 *
 */
 
 void decide_next_order_at_floor(elevator_orders *orders, elevator_controller *controller);
 
+/**
+ * @brief Decides whether the elevator is to stop at the
+ * given floor or not.
+ * 
+ * @param orders An element of elevator_orders providing                            * the order table and priority queue.                                              * @param controller An element of elevator_controller                              * providing the previous direction, previous floor and                             * the present state.  
+ */
 
 void decide_to_stop_at_floor(elevator_orders *orders, elevator_controller *controller);
 
+/**
+ * @brief Controls the floor sensor lights on the floor panel.
+ */
 void control_floor_indicator_lights();
 
+
 /**
-* @brief Drives the elevator in a direction
+* @brief Drives the elevator if not at a floor it is to
+* stop at.
 *
+* @param orders An element of elevator_orders providing                            * the order table and priority queue.                                              * @param controller An element of elevator_controller                              * providing the previous direction, previous floor and                             * the present state. 
 */
-
-
 void driving(elevator_orders *orders, elevator_controller *controller);
 
+/**
+ * @brief Operations connected to stopping at a floor; 
+ * dequeueing the floor, opens and closes the door, and
+ * decides what the next order is to be.
+ * 
+ * @param orders An element of elevator_orders providing                            * the order table and priority queue.                                              * @param controller An element of elevator_controller                              * providing the previous direction, previous floor and                             * the present state.  
+ *
+ */
 void stopping_at_floor(elevator_orders *orders, elevator_controller *controller);
 
+/**
+ * @brief Checks for new orders. When a new element arrives
+ * the queue, decide_next_order_at_floor is called.
+ *
+ * @param orders An element of elevator_orders providing                            * the order table and priority queue.                                              * @param controller An element of elevator_controller                              * providing the previous direction, previous floor and                             * the present state. 
+ *
+ */
 void idle(elevator_orders *orders, elevator_controller *controller);
+
+/**
+ * @brief Initializes the elevator. Reads and acts on stop
+ * signal, and switches between the remaining states.
+ *
+ * @param orders An element of elevator_orders providing                            * the order table and priority queue.                                              * @param controller An element of elevator_controller                              * providing the previous direction, previous floor and                             * the present state. 
+ *
+ */
 
 void control_elevatorNimbus2000(elevator_orders *orders, elevator_controller *controller);
 
-
-//HVA MED LYS???
 
 #endif
