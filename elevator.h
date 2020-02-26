d021d34f10e304c38193cfb9150f2032fa57b565
Hannah Hansen <h.k.hansen97@gmail.com>
2020-02-26 12:07:59 +0100

    doxygen elevator.h

--- elevator.h
+++ elevator.h
@@ -24,50 +24,54 @@ typedef enum {DRIVING_STATE,
 /**
 * @brief Defines an enum with the possible 
 * directions the elevator can hold
-*/
-typedef enum {
+*/typedef enum {
 	DOWN,
 	UP}elevator_direction;
 
-
-/**
-* @brief Defines a struct containing the 
-* previous direction, previous floor and 
-* the present state.
-*/
-
-
+/*! \struct elevator_controller
+ * Defines a struct holding information about the direction, position and state
+ * of the elevator.
+ */
 typedef struct elevator_controller{
-    elevator_direction last_dir;
+	/*! The last known direction of the elevator*/	
+	elevator_direction last_dir;
+	/*! The last known floor of the elevator*/
 	int last_floor;
+	/*! The present elevator_state state of the elevator*/  
 	elevator_state state;
 }elevator_controller;
 
 
 
-
+/**
+ * @brief Bring the elevator to a defined state at a floor
+ *
+ * @param[out] controller When the elevator has reached a defined state, @p last_flo * or of @p controller is updated.
+ */  
 void elevator_initialize(elevator_controller *controller);
 
-/** 
+/**
 * @brief Gets last direction of elevator
 */
 elevator_direction elevator_get_last_direction(elevator_controller *controller);
 
 /**
-* @brief Updates last direction of elevator
-* 
-* @param f Last floor
-*/
+ * @brief Update the last floor @p f of @p controller.
+ * 
+ * @param[in] f Floor to update @p controller. 
+ * @param[out] controller Controller of type elevator_controller.
+ */
 void elevator_update_last_floor(elevator_controller *controller, int f);
 
 
 
 /**
-* @brief Decides new motor direction
+* @brief Set motor direction of elevator_controller @p controller to
+* @p new_direction.
 * 
-* @param new_movement The new direction of 
-* the motor
-*
+* @parameter[in] new_movement The new direction of enum type elevator_direction of 
+* the motor.
+* @param[out] controller Controller whose motor direction is set to @p new_movement. 
 */
 void elevator_set_motor_direction(elevator_controller *controller, HardwareMovement new_movement);
 
@@ -76,20 +80,20 @@ void elevator_set_motor_direction(elevator_controller *controller, HardwareMovem
 * a floor, and updates which floor was the 
 * previous floor.
 *
+* @param[out] controller Controller to uptade last_floor if the elevator is at a floor.
+*
 */
 int elevator_at_floor(elevator_controller *controller);
 
 /**
-* @brief Stops the motor of the elevator
+* @brief Stops the motor of the elevator.
+*
+* @param[in] controller Controller to stop. 
 */
 void elevator_stop_at_floor(elevator_controller *controller);
 
-/**
-* @brief Orders the elevator down if it's at the
-* top, and up if it's at the bottom.
-*
-*/
-int elevator_at_ends();
+
+//int elevator_at_ends();
 
 
 #endif
